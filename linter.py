b185a431d3703cf71bdd17a98edc23be87c3d844
Jacob Birkett <spikespaz@outlook.com>
2018-02-28 08:50:20 -0700

    Revert change to double quotes
    This reverts commit f79069af088b46c400852548fdbd5c703326a6e8.

--- linter.py
+++ linter.py
@@ -14,9 +14,9 @@ This module exports the Shellcheck plugin class.
 Example output with --format gcc
 
 -:230:7: warning: Quote this to prevent word splitting. [SC2046]
--:230:7: note: Useless echo? Instead of "echo $(cmd)", just use "cmd". [SC2005]
+-:230:7: note: Useless echo? Instead of 'echo $(cmd)', just use 'cmd'. [SC2005]
 -:230:158: note: Double quote to prevent globbing and word splitting. [SC2086]
--:234:10: error: Add double quotes around ${VAR[@]}, otherwise it"s just like $* and breaks on spaces. [SC2068]
+-:234:10: error: Add double quotes around ${VAR[@]}, otherwise it's just like $* and breaks on spaces. [SC2068]
 
 """
 
@@ -27,20 +27,20 @@ from os import name
 class Shellcheck(Linter):
     """Provides an interface to shellcheck."""
 
-    syntax = ("shell-unix-generic", "bash")
-    cmd = "shellcheck --format gcc -"
+    syntax = ('shell-unix-generic', 'bash')
+    cmd = 'shellcheck --format gcc -'
 
     if name == "nt":
         cmd = "wsl " + cmd
 
     regex = (
-        r"^.+?:(?P<line>\d+):(?P<col>\d+): "
-        r"(?:(?P<error>error)|(?P<warning>(warning|note))): "
-        r"(?P<message>.+)$"
+        r'^.+?:(?P<line>\d+):(?P<col>\d+): '
+        r'(?:(?P<error>error)|(?P<warning>(warning|note))): '
+        r'(?P<message>.+)$'
     )
 
     defaults = {
-        "--exclude=,": ""
+        '--exclude=,': ''
     }
-    inline_overrides = "exclude"
-    comment_re = r"\s*#"
+    inline_overrides = 'exclude'
+    comment_re = r'\s*#'
